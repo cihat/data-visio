e075e25ce8c4b7c0f52048a409311b09a8f4d61a
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-03-08 00:55:18 +0300

    Fix importing for all ant design components

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,63 +1,18 @@
 import 'ant-design-vue/dist/reset.css';
 // import './assets/main.css'
 
+import App from './App.vue'
+import router from './router'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import HighchartsVue from 'highcharts-vue';
-
-import {
-  Button,
-  Input,
-  Layout,
-  Row,
-  Col,
-  Comment,
-  Tooltip,
-  Avatar,
-  Card,
-  Tag,
-  Radio,
-  Menu,
-  Form,
-  Alert,
-  Empty,
-  Tabs,
-  Pagination,
-  Space,
-  Breadcrumb,
-} from 'ant-design-vue'
-
-const components = [
-  Button,
-  Input,
-  Layout,
-  Row,
-  Col,
-  Comment,
-  Tooltip,
-  Avatar,
-  Card,
-  Tag,
-  Radio,
-  Menu,
-  Form,
-  Alert,
-  Empty,
-  Tabs,
-  Pagination,
-  Space,
-  Breadcrumb,
-]
-
-
-import App from './App.vue'
-import router from './router'
+import Antd from 'ant-design-vue';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(HighchartsVue)
-components.forEach(c => app.use(c))
+app.use(Antd)
 
 app.mount('#app')
