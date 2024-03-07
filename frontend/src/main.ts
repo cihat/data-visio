01833b07b11a66de6aee9f2782391f007e53e733
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-03-08 00:09:51 +0300

    Add `ant-design-vue/icons, highcharts-vue` packages

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,54 @@
-import './assets/main.css'
+import 'ant-design-vue/dist/reset.css';
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import HighchartsVue from 'highcharts-vue';
+
+import {
+  Button,
+  Input,
+  Layout,
+  Row,
+  Col,
+  Comment,
+  Tooltip,
+  Avatar,
+  Card,
+  Tag,
+  Radio,
+  Menu,
+  Form,
+  Alert,
+  Empty,
+  Tabs,
+  Pagination,
+  Space,
+  Breadcrumb,
+} from 'ant-design-vue'
+
+const components = [
+  Button,
+  Input,
+  Layout,
+  Row,
+  Col,
+  Comment,
+  Tooltip,
+  Avatar,
+  Card,
+  Tag,
+  Radio,
+  Menu,
+  Form,
+  Alert,
+  Empty,
+  Tabs,
+  Pagination,
+  Space,
+  Breadcrumb,
+]
+
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +57,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(HighchartsVue)
+components.forEach(c => app.use(c))
 
 app.mount('#app')
---
+++ frontend/src/views/ChartView.vue
@@ -0,0 +1,98 @@
+<script lang="ts" setup>
+import { ref, reactive } from 'vue';
+const selectedKeys2 = ref<string[]>(['1']);
+const openKeys = ref<string[]>(['sub1']);
+
+const chartOptions = reactive({
+  chart: {
+    type: 'spline'
+  },
+  title: {
+    text: 'Test Dashboard'
+  },
+  series: [{
+    data: [10, 0, 8, 2, 6, 4, 5, 5]
+  }],
+  accessibility: {
+    description: 'This is a description of the entire chart.'
+  }
+})
+</script>
+
+<template>
+  <a-layout>
+    <a-layout-content style="padding: 0 50px">
+      <a-breadcrumb style="margin: 16px 0">
+        <a-breadcrumb-item>Home</a-breadcrumb-item>
+        <a-breadcrumb-item>List</a-breadcrumb-item>
+        <a-breadcrumb-item>App</a-breadcrumb-item>
+      </a-breadcrumb>
+      <a-layout style="padding: 24px 0; background: #fff">
+        <a-layout-sider width="200" style="background: #fff">
+          <a-menu v-model:selectedKeys="selectedKeys2" v-model:openKeys="openKeys" mode="inline" style="height: 100%">
+            <a-sub-menu key="sub1">
+              <template #title>
+                <span>
+                  subnav 1
+                </span>
+              </template>
+              <a-menu-item key="1">option1</a-menu-item>
+              <a-menu-item key="2">option2</a-menu-item>
+              <a-menu-item key="3">option3</a-menu-item>
+              <a-menu-item key="4">option4</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub2">
+              <template #title>
+                <span>
+                  subnav 2
+                </span>
+              </template>
+              <a-menu-item key="5">option5</a-menu-item>
+              <a-menu-item key="6">option6</a-menu-item>
+              <a-menu-item key="7">option7</a-menu-item>
+              <a-menu-item key="8">option8</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub3">
+              <template #title>
+                <span>
+                  subnav 3
+                </span>
+              </template>
+              <a-menu-item key="9">option9</a-menu-item>
+              <a-menu-item key="10">option10</a-menu-item>
+              <a-menu-item key="11">option11</a-menu-item>
+              <a-menu-item key="12">option12</a-menu-item>
+            </a-sub-menu>
+          </a-menu>
+        </a-layout-sider>
+        <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
+          Chart
+          <highcharts :options="chartOptions"></highcharts>
+
+        </a-layout-content>
+      </a-layout>
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      2024 © Cihat SALIK
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<style scoped>
+#components-layout-demo-top-side .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+</style>
